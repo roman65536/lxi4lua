80a410014e1c3b357ebb1ec3d848d05da29fb252
Roman Pollak <roman65536@yahoo.com>
2026-05-24 19:43:38 +0200

    Readme changed

--- examples/test.lua
+++ examples/test.lua
@@ -1,3 +1,8 @@
+require "os"
+
+function sleep(n)
+  os.execute("sleep " .. tonumber(n))
+end
 
 function text_prompt(msg)
   io.write(msg)
@@ -17,4 +22,20 @@ addr=text_prompt("Hostname or Ip address of SCPI/LXI Device")
 dev=lxi_connect(addr,nil, nil, 2000, "RAW")
 dev_id=lxi_scpi(dev,"*IDN?")
 print(dev_id)
+
+for a=1,10 do
+lxi_scpi(dev,string.format("SOURce0:FREQ %d",a*1000))
+sleep(1)
+end
+
+for a=0,10 do
+lxi_scpi(dev,string.format("OUTP0 ON"))
+sleep(1)
+lxi_scpi(dev,string.format("OUTP1 ON"))
+sleep(1)
+lxi_scpi(dev,string.format("OUTP0 OFF"))
+sleep(1)
+lxi_scpi(dev,string.format("OUTP1 OFF"))
+sleep(1)
+end
 
